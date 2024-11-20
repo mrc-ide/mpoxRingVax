7832f465780d9c3d75e3bec91fc568ebef101317
Charlie Whittaker <cwhittaker1000@gmail.com>
2024-11-20 16:55:00 +0000

    ready to test

--- R/mpox_ring_vaccination_sim.R
+++ R/mpox_ring_vaccination_sim.R
@@ -62,9 +62,46 @@ generation_time <- function(n) { rep(10, n)}
 vaccine_coverage <- 0.5
 vaccine_efficacy_infection <- 0.8
 prob_quarantine <- 0.8
-onset_to_quarantine <- 0.1
+onset_to_quarantine <- function(n) { rep(0.1, n) }
 quarantine_efficacy_household <- 0.77
 quarantine_efficacy_else <- 0.6
+vaccine_start <- 0
+prop_asymptomatic <- 0.1
+
+x <- basic_ring_vaccination_sim(mn_offspring_sexual_SW = mn_offspring_sexual_SW,
+                                disp_offspring_sexual_SW = disp_offspring_sexual_SW,
+                                mn_offspring_sexual_PBS = mn_offspring_sexual_PBS,
+                                disp_offspring_sexual_PBS = disp_offspring_sexual_PBS,
+                                mn_offspring_sexual_genPop = mn_offspring_sexual_genPop,
+                                disp_offspring_sexual_genPop = disp_offspring_sexual_genPop,
+                                sexual_transmission_occupation_matrix = sexual_transmission_occupation_matrix,
+                                mn_offspring_hh = mn_offspring_hh,
+                                disp_offspring_hh = disp_offspring_hh,
+                                mn_offspring_community = mn_offspring_community,
+                                disp_offspring_community = disp_offspring_community,
+                                community_transmission_age_matrix = community_transmission_age_matrix,
+                                prop_asymptomatic = prop_asymptomatic,
+                                generation_time = generation_time,
+                                infection_to_onset = infection_to_onset,
+                                vaccine_start = vaccine_start,
+                                vaccine_coverage = vaccine_coverage,
+                                vaccine_efficacy_infection = vaccine_efficacy_infection,
+                                vaccine_efficacy_transmission = vaccine_efficacy_transmission,
+                                vaccine_logistical_delay = vaccine_logistical_delay,
+                                vaccine_protection_delay = vaccine_protection_delay,
+                                prob_quarantine = prob_quarantine,
+                                onset_to_quarantine = onset_to_quarantine,
+                                quarantine_efficacy_household = quarantine_efficacy_household,
+                                quarantine_efficacy_else = quarantine_efficacy_else,
+                                synthetic_household_df = synthetic_household_df,
+                                t0 = t0,
+                                population = population,
+                                check_final_size = check_final_size,
+                                initial_immune = initial_immune,
+                                seeding_cases = seeding_cases,
+                                seed = seed)
+
+
 
 #' @export
 basic_ring_vaccination_sim <- function(## Sexual Transmission Parameters
